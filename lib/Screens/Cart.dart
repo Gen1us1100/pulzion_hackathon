a87bbf3d68808dcf3edf83f6bb02e487e31b3df7
Pushkar Tipre <pushkartipre53@gmail.com>
2023-05-01 22:00:43 +0530

    Initial Commit - Pushkar Day 1

--- lib/Screens/Cart.dart
+++ lib/Screens/Cart.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Widgets/CartAppBar.dart';
+
 class Cart extends StatelessWidget {
   const Cart({Key? key}) : super(key: key);
 
@@ -9,7 +11,6 @@ class Cart extends StatelessWidget {
       body: ListView(
         children: [
           CartAppBar(),
-
           Container(
             height: 700,
           )
