f1d1bb2e685fc913183bfaadb3d487c5dc687354
Dell <omkarkatkar2004@gmail.com>
2023-05-03 00:46:38 +0530

    commit omkar 2

--- lib/Screens/ItemPage.dart
+++ lib/Screens/ItemPage.dart
@@ -6,7 +6,7 @@ class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF4C53A5),
+        backgroundColor: Color(0xFFFCE4EC),
         body: ListView(
           children: [
             ItemAppBar(),
@@ -18,21 +18,41 @@ class ItemPage extends StatelessWidget {
               height: 20,
             ),
             Container(
+              decoration: BoxDecoration(color: Color(0xFFF8BBD0)),
               height: 100,
               width: 100,
-              child: Text(
-                "AIRPODS ",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+              child: Center(
+                child: Text(
+                  "AIRPODS - @900  ",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                ),
               ),
             ),
             SizedBox(height: 30),
-            Container(
-              height: 100,
-              width: 100,
-              child: Text(
-                "Airpod is good ",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                decoration: BoxDecoration(color: Color(0xFFFFCDD2)),
+                height: 50,
+                width: 100,
+                child: Center(
+                  child: Text(
+                    "DESCRIPTION ",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                ),
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                  decoration: BoxDecoration(color: Color(0xFFFFCDD2)),
+                  height: 200,
+                  width: 100,
+                  child: Text(
+                    " AirPods with Charging Case.More magical than ever.The new AirPods combine intelligent design with breakthrough technology and crystal clear sound. Powered by the new Apple H1 headphone chip, AirPods now feature hands-free access to Siri using just your voice. And up to 3 hours of talk time on a single charge.",
+                    style: TextStyle(fontSize: 20),
+                  )),
             ),
           ],
         ));
